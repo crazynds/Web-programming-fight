2d2379fa8c33cc3f93ff8a8a69d4875b87ebd63f
Crazynds <lh.lagonds@gmail.com>
2023-11-26 22:44:21 -0300

    fix: corrigido rotas que estavam com nome duplicado e volume do redis removido

--- routes/web.php
+++ routes/web.php
@@ -64,8 +64,6 @@ Route::middleware('auth')->group(function(){
         ->name('run.rejudge');
     Route::get('/run/global/live',[SubmitRunController::class,'global'])
         ->name('run.global');
-    Route::get('/run/{submitRun}/show',[SubmitRunController::class,'show'])
-        ->name('run.show');
     Route::get('/run/{submitRun}/download',[SubmitRunController::class,'download'])
         ->name('run.download');
 
