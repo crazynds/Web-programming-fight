5e3ca315a3a4b036bd68600865b22e1586c42f3a
Crazynds <lh.lagonds@gmail.com>
2023-12-06 19:54:32 -0300

    feat: sistema de confetes e removido sistema de reload na pagina de submissoes #29

--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:web')->group(function(){
     Route::get('/run/{submitRun}/code',[SubmitRunController::class,'getCode'])
         ->name('submitRun.code');
+    Route::get('/run/{submitRun}/result',[SubmitRunController::class,'result'])
+        ->name('submitRun.result');
 });
