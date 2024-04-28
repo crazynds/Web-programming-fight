3803394d25d380b7ffa0e8bbd274ac0e20465e4d
crazynds <lh.lagonds@gmail.com>
2024-04-28 13:53:52 -0300

    feat: adicionado reverb

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -42,3 +42,10 @@ window.addEventListener('load', () => {
         $recaptcha.setAttribute('required', 'required');
     }
 })
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import './echo';
