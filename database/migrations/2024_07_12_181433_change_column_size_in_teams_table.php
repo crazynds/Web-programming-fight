6e633f44fe8b9893cb4815cee2c9106c21396f53
Crazynds <luli.broch@gmail.com>
2024-07-12 18:20:19 -0300

    fix: adicionado acronimo da instituicao e aumentado o nome dos times

---
+++ database/migrations/2024_07_12_181433_change_column_size_in_teams_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->string('name', 255)->change();
+            $table->string('institution_acronym', 10)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->string('name', 255)->change();
+            $table->dropColumn('institution_acronym');
+        });
+    }
+};
