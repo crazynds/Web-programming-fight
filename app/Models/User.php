ebb7cb33d4c71fd6de4f2857dd46ed2fc55111b4
Crazynds <luli.broch@gmail.com>
2024-07-17 14:47:12 -0300

    feat: dado permissao para o pozzer

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
     public function isAdmin()
     {
         //return false;
-        return $this->id == 1;
+        return $this->id == 1 || $this->id == 22;
     }
 
     public function problems()
