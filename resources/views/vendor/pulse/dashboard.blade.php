7247bc7e592997d399ed0c4fef757292c925468b
crazynds <lh.lagonds@gmail.com>
2024-07-18 06:27:11 -0300

    refactor: removido active sessions e ajustado o pulse

--- resources/views/vendor/pulse/dashboard.blade.php
+++ resources/views/vendor/pulse/dashboard.blade.php
@@ -1,7 +1,8 @@
 <x-pulse>
 
     <livewire:requests-graph cols="8" />
-    <livewire:pulse_active_session cols="4" />
+
+    <livewire:pulse.slow-requests cols="4" />
 
     <livewire:pulse.usage cols="4" rows="2" />
 
@@ -13,12 +14,8 @@
 
     <livewire:pulse.exceptions cols="4" />
 
-    <livewire:pulse.slow-requests cols="4" rows="2" />
-
-
     <livewire:pulse.slow-jobs cols="4" rows="2" />
 
     <livewire:pulse.table-info cols="4" rows="2" />
-
 
 </x-pulse>
