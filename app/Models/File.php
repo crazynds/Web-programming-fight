012dd8e49ef78c2d102ecd7aa9f142ae69966a35
crazynds <luli.broch@gmail.com>
2025-05-18 13:31:57 -0300

    fix: corrigido extract que estava alterando a variavel de maneira errada

--- app/Models/File.php
+++ app/Models/File.php
@@ -3,67 +3,72 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use \Illuminate\Http\UploadedFile;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use STS\ZipStream\ZipStream;
 
 class File extends Model
 {
     public $timestamps = false;
+
     public $guarded = [];
 
     // Save at max 16KB in database of content
     const MAX_DB_CONTENT = 16 * 1024;
 
-
     public static function createFile(UploadedFile $upfile, string $path, bool $forceDisk = false, bool $preventCompact = false)
     {
-        $file = new File();
+        $file = new File;
         $preventCompact |= $forceDisk;
         $storeInDb = false;
-        if ($upfile->getSize() < self::MAX_DB_CONTENT * 2 && !$preventCompact) {
-            $file->path = $path . '/' . $upfile->hashName() . '_db';
+        if ($upfile->getSize() < self::MAX_DB_CONTENT * 2 && ! $preventCompact) {
+            $file->path = $path.'/'.$upfile->hashName().'_db';
             $file->content = $upfile->get();
             $file->compact();
             if (strlen($file->content) > self::MAX_DB_CONTENT) {
                 $file->compacted = false;
                 $file->content = null;
                 $storeInDb = false;
-            } else $storeInDb = true;
+            } else {
+                $storeInDb = true;
+            }
         }
-        if (!$storeInDb) {
+        if (! $storeInDb) {
             $file->path = $upfile->store($path);
             $file->content = null;
         }
 
         $file->size = $upfile->getSize();
         $file->type = $upfile->getClientOriginalExtension();
-        $file->hash = hash_file("sha256", $upfile->getPathname());
+        $file->hash = hash_file('sha256', $upfile->getPathname());
         $file->save();
+
         return $file;
     }
 
     public static function createFileByData($data, string $path, bool $forceDisk = false, bool $preventCompact = false)
     {
-        $file = new File();
+        $file = new File;
         $preventCompact |= $forceDisk;
         $storeInDb = false;
 
-        $hashName = sha1(hash('sha256', $data) . '-' . strlen($data) . '-' . random_bytes(4));
+        $hashName = sha1(hash('sha256', $data).'-'.strlen($data).'-'.random_bytes(4));
 
-        if (strlen($data) < self::MAX_DB_CONTENT * 2 && !$preventCompact) {
-            $file->path = $path . '/' . $hashName . '_db';
+        if (strlen($data) < self::MAX_DB_CONTENT * 2 && ! $preventCompact) {
+            $file->path = $path.'/'.$hashName.'_db';
             $file->content = $data;
             $file->compact();
             if (strlen($file->content) > self::MAX_DB_CONTENT) {
                 $file->compacted = false;
                 $file->content = null;
                 $storeInDb = false;
-            } else $storeInDb = true;
+            } else {
+                $storeInDb = true;
+            }
         }
-        if (!$storeInDb) {
-            $file->path = $path . '/' . $hashName;
-            Storage::put($path . '/' . $hashName, $data);
+        if (! $storeInDb) {
+            $file->path = $path.'/'.$hashName;
+            Storage::put($path.'/'.$hashName, $data);
             $file->content = null;
         }
 
@@ -71,28 +76,27 @@ class File extends Model
         $file->type = 'blob';
         $file->hash = hash('sha256', $data);
         $file->save();
+
         return $file;
     }
 
-
-
     public static function createFileByStream($stream, int $size, string $hash, string $path, bool $forceDisk = false)
     {
-        $file = new File();
+        $file = new File;
 
-        $hashName = sha1($hash . '-' . $size . '-' . random_bytes(4));
+        $hashName = sha1($hash.'-'.$size.'-'.random_bytes(4));
 
-        if ($size < self::MAX_DB_CONTENT && !$forceDisk) {
-            $file->path = $path . '/' . $hashName . '_streamed_db';
+        if ($size < self::MAX_DB_CONTENT && ! $forceDisk) {
+            $file->path = $path.'/'.$hashName.'_streamed_db';
             $file->content = '';
-            do{
+            do {
                 $file->content .= fread($stream, $size);
-            }while(strlen($file->content) < $size);
+            } while (strlen($file->content) < $size);
             $file->compact();
             fclose($stream);
         } else {
-            Storage::put($path . '/' . $hashName, $stream);
-            $file->path = $path . '/' . $hashName;
+            Storage::put($path.'/'.$hashName, $stream);
+            $file->path = $path.'/'.$hashName;
             $file->content = null;
         }
 
@@ -100,9 +104,9 @@ class File extends Model
         $file->type = '';
         $file->hash = $hash;
         $file->save();
+
         return $file;
     }
-
 
     public function download(string $title)
     {
@@ -122,8 +126,9 @@ class File extends Model
         } else {
             if ($this->compacted) {
                 return gzuncompress($this->content);
-            } else
+            } else {
                 return $this->content;
+            }
         }
     }
 
@@ -135,15 +140,18 @@ class File extends Model
             $stream = fopen('php://memory', 'r+');
             fwrite($stream, $this->get());
             rewind($stream);
+
             return $stream;
         }
     }
 
     public function addToZip(ZipStream $zip, $pathName = null)
     {
-        if (is_null($pathName)) $pathName = $this->path;
+        if (is_null($pathName)) {
+            $pathName = $this->path;
+        }
 
-        if (!is_null($this->content)) {
+        if (! is_null($this->content)) {
             $zip->addRaw($this->get(), $pathName);
         } else {
             $zip->add($this->url(), $pathName);
@@ -161,27 +169,32 @@ class File extends Model
             case 'local':
                 $url = Storage::url($this->path);
         }
+
         return $url;
     }
 
     public function compact()
     {
-        if (is_null($this->content))
+        if (is_null($this->content)) {
             return;
-        if ($this->compacted)
+        }
+        if ($this->compacted) {
             return;
+        }
         $this->compacted = true;
         $this->content = gzcompress($this->content, 9);
     }
 
     public function extract()
     {
-        if (is_null($this->content))
+        if (is_null($this->content)) {
             return;
-        if (!$this->compacted)
+        }
+        if (! $this->compacted) {
             return;
+        }
 
-        $this->compacted = true;
+        $this->compacted = false;
         $this->content = gzuncompress($this->content);
     }
 }
