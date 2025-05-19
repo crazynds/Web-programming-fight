e26c3d6d071bb671e2dfc52511863ac64b2d1939
crazynds <luli.broch@gmail.com>
2025-05-19 14:41:25 -0300

    fix: correcao do python 3.13 no executavel correto

--- docker/config/runPython3.11.sh
+++ docker/config/runPython3.11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Caminho alternativo do Python
-PYTHON_ALT="/langs/python3.11/bin/python"
+PYTHON_ALT="/langs/python3.11/bin/python3.11"
 
 # Verifica se o comando python3.11 existe no PATH
 if command -v python3.11 >/dev/null 2>&1; then
@@ -9,6 +9,7 @@ if command -v python3.11 >/dev/null 2>&1; then
 elif [ -x "$PYTHON_ALT" ]; then
     "$PYTHON_ALT" -u /var/config/exec "$@"
 else
+    echo "Não foi possível encontrar o executavel Python 3.11."
     exit 1
 fi
 
--- docker/config/runPython3.13.sh
+++ docker/config/runPython3.13.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Caminho alternativo do Python
-PYTHON_ALT="/langs/python3.13/bin/python"
+PYTHON_ALT="/langs/python3.13/bin/python3.13"
 
 # Verifica se o comando python3.13 existe no PATH
 if command -v python3.13 >/dev/null 2>&1; then
@@ -9,6 +9,7 @@ if command -v python3.13 >/dev/null 2>&1; then
 elif [ -x "$PYTHON_ALT" ]; then
     "$PYTHON_ALT" -u /var/config/exec "$@"
 else
+    echo "Não foi possível encontrar o executavel Python 3.13."
     exit 1
 fi
 
